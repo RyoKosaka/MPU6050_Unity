1e8517d091f976ecee35c0d5f0ada6702e6f2590
Ryo Kosaka <r.k1227@gmail.com>
2018-12-01 16:11:03 +0900

    Add files via upload

---
+++ Assets/MPU6050/Script/mpu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mpu: MonoBehaviour
+{
+
+    public SerialHandler serialHandler;
+    public GameObject NorthStar;
+
+    // Use this for initialization
+    void Start()
+    {
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnDataReceived(string message)
+    {
+        try
+        {
+            string[] angles = message.Split(',');
+            //Vector3 angle = new Vector3(float.Parse(angles[0]), float.Parse(angles[2]), float.Parse(angles[1]));
+            //cube.transform.rotation = Quaternion.Euler(angle);
+            NorthStar.transform.rotation = new Quaternion(float.Parse(angles[0]), float.Parse(angles[1]), float.Parse(angles[3]), float.Parse(angles[2]));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
